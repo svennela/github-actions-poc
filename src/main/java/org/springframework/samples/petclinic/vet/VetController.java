db5e8e15ddfc374296b7f12adf551602a02831eb
Sridhar Vennela <svennela@vmware.com>
2023-08-14 14:07:50 -0700

    udpate Vet Controller

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -73,6 +73,7 @@ class VetController {
 		// objects so it is simpler for JSon/Object mapping
 		Vets vets = new Vets();
 		vets.getVetList().addAll(this.vets.findAll());
+		System.out.print("-----------");
 		return vets;
 	}
 
